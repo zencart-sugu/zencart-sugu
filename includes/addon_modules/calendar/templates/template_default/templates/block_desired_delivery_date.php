cbd427193f072ead1d1b9404ee787202d3f46cd7
kohata <kohata@e7-ware.com>
2009-10-29 14:35:15 +0900

    既存のaddonモジュールをすぐでき用に用意する calendarモジュール refs #7482

---
+++ includes/addon_modules/calendar/templates/template_default/templates/block_desired_delivery_date.php
@@ -0,0 +1,2 @@
+<h3 class="headline"><?php echo MODULE_CALENDAR_HOPE_DELIVERY_DAY_HEADER; ?></h3>
+<p><?php echo zen_draw_pull_down_menu('calendar_hope_delivery_day', $option, $_SESSION['calendar_hope_delivery_day']); ?></p>
