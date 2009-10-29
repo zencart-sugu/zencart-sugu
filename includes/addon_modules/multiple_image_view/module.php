f8eded5e1b5448ec26d97647bf6148b3ce402b01
ohtsuji <ohtsuji@okra.(none)>
2009-10-29 15:52:27 +0900

    バージョンナンバーの変更

--- includes/addon_modules/multiple_image_view/module.php
+++ includes/addon_modules/multiple_image_view/module.php
@@ -19,7 +19,7 @@ if (!defined('IS_ADMIN_FLAG')) {
 
     var $author                        = "Otsuji Takashi";
     var $author_email                  = "info@zencart-sugu.jp";
-    var $version                       = "0.1";
+    var $version                       = "0.1.1";
     var $require_zen_cart_version      = "1.3.0.2";
     var $require_addon_modules_version = "0.1";
 
