e3632c5507c1633b77680688fddfa7925da13e84
kohata <kohata@e7-ware.com>
2009-11-04 22:38:14 +0900

    すぐでき glovalnavi

---
+++ includes/addon_modules/globalnavi/configure.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+define('MODULE_GLOBALNAVI_STATUS_DEFAULT', 'true');
+define('MODULE_GLOBALNAVI_SORT_ORDER_DEFAULT', '1950');
+define('MODULE_GLOBALNAVI_LIMIT_DEFAULT', '5');
+
