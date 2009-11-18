e2045ef2876054b403874d52a418ed415f21ef1c
kohata <kohata@e7-ware.com>
2009-11-18 19:37:12 +0900

    すぐできreviews

---
+++ includes/addon_modules/reviews/templates/template_default/templates/page_product_reviews_write_complete.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * addon_modules block Template
+ *
+ * @package templateSystem
+ * @copyright Copyright (C) 2009 Liquid System Technology, Inc.
+ * @author Koji Sasaki
+ * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: block.php $
+ */
+
+?>
+<h1>•Ï•”•Â°º≈Íπ∆¥∞Œª</h1>
+<p>•Ï•”•Â°º≈Íπ∆§Ú¥∞Œª§∑§ﬁ§∑§ø°£</p>
