435b6d9b3157af2f298a64c6d70cbed2d93ec6cd
kohata <kohata@e7-ware.com>
2009-11-19 11:10:18 +0900

    すぐできeasy_design 書き込み権限チェック追加

---
+++ includes/addon_modules/easy_design/languages/japanese.php
@@ -0,0 +1,45 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2009 Liquid System Technology, Inc.                    |
+// | Author Koji Sasaki                                                   |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 The zen-cart developers                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: japanese.php $
+//
+define('MODULE_EASY_DESIGN_TITLE',                    'デザインの設定');
+define('MODULE_EASY_DESIGN_DESCRIPTION',              'テンプレート、色、ロゴを簡単に変更できるようにする');
+
+define('MODULE_EASY_DESIGN_STATUS_TITLE',             'デザインの設定の有効化');
+define('MODULE_EASY_DESIGN_STATUS_DESCRIPTION',       'デザインの設定を有効にしますか？ <br />true: 有効<br />false: 無効');
+
+define('MODULE_EASY_DESIGN_SORT_ORDER_TITLE',         '優先順');
+define('MODULE_EASY_DESIGN_SORT_ORDER_DESCRIPTION',   'モジュールの優先順を設定できます。数字が小さいほど先にモジュールの読み込みと処理が実行されます。半角数字で他のモジュールと重ならないように設定してください。');
+
+// addon_moduleブロック管理用
+define('MODULE_EASY_DESIGN_BLOCK_TITLE',              'デザインの設定');
+
+define('BOX_EASY_DESIGN',                             'デザインの設定');
+
+define('EASY_DESIGN_TAGLINE_NAME',                    'タグライン');
+define('EASY_DESIGN_TAGLINE_VALUE',                   'ここにタグラインを入力してください。');
+define('EASY_DESIGN_COPYLIGHT_NAME',                  'コピーライト');
+define('EASY_DESIGN_COPYLIGHT_VALUE',                 'ここにコピーライトを入力してください。');
+define('EASY_DESIGN_DEFAULT_MAIN_COLOR_NAME',         'メインカラー');
+define('EASY_DESIGN_DEFAULT_SUB_COLOR_NAME',          'サブカラー');
+
+?>
