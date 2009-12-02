dcf48f715914315e5e1157a410b2d5bf4e95aef8
Otsuji Takashi <o2gtkc@gmail.com>
2009-12-02 12:26:53 +0900

    Revert "バージョン変更"
    This reverts commit e9c37226533b16f2168c13072864742257c137c4.

--- includes/addon_modules/easy_admin_simplify/module.php
+++ includes/addon_modules/easy_admin_simplify/module.php
@@ -46,7 +46,7 @@ if (!defined('IS_ADMIN_FLAG')) {
 
     var $author                        = "kohata";
     var $author_email                  = "info@zencart-sugu.jp";
-    var $version                       = "0.1.3";
+    var $version                       = "0.1";
     var $require_zen_cart_version      = "1.3.0.2";
     var $require_addon_modules_version = "0.1";
 
