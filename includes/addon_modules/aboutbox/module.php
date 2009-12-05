16bf918b9d157759f990f3407795d481cc5b8fe3
saito <saito>
2009-12-05 16:40:48 +0900

    version番号変更

--- includes/addon_modules/aboutbox/module.php
+++ includes/addon_modules/aboutbox/module.php
@@ -18,7 +18,7 @@ if (!defined('IS_ADMIN_FLAG')) {
   class aboutbox extends addOnModuleBase {
     var $author = 'saito';
     var $author_email = 'info@zencart-sugu.jp';
-    var $version = '0.0.1';
+    var $version = '0.1.2';
     var $require_zen_cart_version = '1.3.0.2';
     var $require_addon_modules_version = '1.0.0';
 
