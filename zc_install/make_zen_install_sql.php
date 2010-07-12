3d2522db3a0b30665f3bd3b9684cba3f64e76b2c
Tsunemasa Hachiya <hachiya@ark-web.jp>
2010-07-12 17:47:24 +0900

    インストーラー用sql生成

---
+++ zc_install/make_zen_install_sql.php
@@ -0,0 +1,50 @@
+<?php
+	$sql_file = "sugudeki.sql";
+	$lines = file($sql_file);
+	$insert = "";
+	$other = "";
+
+	foreach($lines as $line) {
+		$line = trim($line);
+/*		$encoding = mb_detect_encoding($line);
+		$line = mb_convert_encoding($line, "EUC-JP", $encoding);*/
+		$line = str_replace("`", "", $line);
+		$line_upper = strtoupper($line);
+
+		if(substr($line_upper, 0, 12) == 'INSERT INTO '){
+		//if(preg_match("/INSERT INTO /", $line_upper)){
+			//if(!preg_match("/(`media_types`)|(`paypal_payment_status`)|(`template_select`)|(`address_format`)|(`admin`)|(`banners`)|(`configuration`)|(`configuration_group`)|(`countries`)|(`currencies`)|(`languages`)|(`layout_boxes`)|(`orders_status`)|(`product_types`)|(`products_options_types`)|(`products_options_values`)|(`zones`)|(`product_type_layout`)|(`query_builder`)|(`get_terms_to_filter`)|(`project_version`)|(`project_version_history`)|(`tax_rates`)|(`geo_zones`)|(`zones_to_geo_zones`)|(`tax_class`)/", $line)) {
+			if(!preg_match("/(media_types)|(paypal_payment_status)|(template_select)|(address_format)|(admin)|(banners)|(configuration)|(configuration_group)|(countries)|(currencies)|(languages)|(layout_boxes)|(orders_status)|(product_types)|(products_options_types)|(products_options_values)|(zones)|(product_type_layout)|(query_builder)|(get_terms_to_filter)|(project_version)|(project_version_history)|(tax_rates)|(geo_zones)|(zones_to_geo_zones)|(tax_class)/", $line)) {
+				$insert .= $line."\n";
+			}else{
+				$other .= $line."\n";
+			}
+		}else{
+			if(!preg_match("/(LOCK TABLES)|(UNLOCK TABLES)|(!40000 ALTER TABLE )/", $line)) {
+				$other .= $line."\n";
+			}
+		}
+	}
+/*
+	$encoding = mb_detect_encoding($insert);
+	$insert = mb_convert_encoding($insert, "EUC-JP", $encoding);
+
+	$encoding = mb_detect_encoding($other);
+	$other = mb_convert_encoding($other, "EUC-JP", $encoding);*/
+
+	umask(0);
+	$file_name = "mysql_zencart.sql";//作成するファイル名
+	$file_pointer = fopen($file_name, "w+");
+	flock($file_pointer, LOCK_EX);
+	fputs($file_pointer, $other);
+	flock($file_pointer, LOCK_UN);
+	fclose($file_pointer);
+
+	umask(0);
+	$file_name = "demo/mysql_demo.sql";//作成するファイル名
+	$file_pointer = fopen($file_name, "w+");
+	flock($file_pointer, LOCK_EX);
+	fputs($file_pointer, $insert);
+	flock($file_pointer, LOCK_UN);
+	fclose($file_pointer);
+?>
