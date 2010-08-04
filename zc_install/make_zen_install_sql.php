741a31a5b327031fc47b2ca3f077434b30238331
Tsunemasa Hachiya <hachiya@ark-web.jp>
2010-08-04 19:02:16 +0900

    demoにもデフォルトにも必要ないレコードは除外

--- zc_install/make_zen_install_sql.php
+++ zc_install/make_zen_install_sql.php
@@ -30,11 +30,13 @@
 			}
 		}*/
 
-		//デモの挿入
 		if(substr($line_upper, 0, 12) == 'INSERT INTO '){
-			if(preg_match("/(`address_book`)|(`categories`)|(`categories_description`)|(`configuration`)|(`customers`)|(`customers_info`)|(`ezpages`)|(`featured`)|(`group_pricing`)|(`manufacturers`)|(`manufacturers_info`)|(`media_clips`)|(`media_manager`)|(`media_to_products`)|(`meta_tags_categories_description`)|(`meta_tags_products_description`)|(`music_genre`)|(`product_music_extra`)|(`product_types_to_category`)|(`products`)|(`products_attributes`)|(`products_attributes_download`)|(`products_description`)|(`products_discount_quantity`)|(`products_options`)|(`products_options_values`)|(`products_options_values_to_products_options`)|(`products_to_categories`)|(`record_artists`)|(`record_artists_info`)|(`record_company`)|(`record_company_info`)|(`reviews`)|(`reviews_description`)|(`salemaker_sales`)|(`specials`)/", $line)) {
+			//デモ用データ
+			if(preg_match("/(`categories`)|(`categories_description`)|(`ezpages`)|(`featured`)|(`group_pricing`)|(`manufacturers`)|(`manufacturers_info`)|(`media_clips`)|(`media_manager`)|(`media_to_products`)|(`meta_tags_categories_description`)|(`meta_tags_products_description`)|(`music_genre`)|(`product_music_extra`)|(`product_types_to_category`)|(`products`)|(`products_attributes`)|(`products_attributes_download`)|(`products_description`)|(`products_discount_quantity`)|(`products_options`)|(`products_options_values`)|(`products_options_values_to_products_options`)|(`products_to_categories`)|(`record_artists`)|(`record_artists_info`)|(`record_company`)|(`record_company_info`)|(`reviews`)|(`reviews_description`)|(`salemaker_sales`)|(`specials`)/", $line)) {
 				$demo .= $line."\n";
-			}else{
+
+			//デフォルトデータ
+			}elseif(!preg_match("/(`orders`)|(`orders_products`)|(`orders_products_attributes`)|(`orders_total`)|(`orders_status_history`)|(`customers`)|(`customers_info`)|(`customers_basket`)|(`address_book`)|(`admin_activity_log`)|(`banners_history`)|(`counter_history`)|(`coupon_email_track`)|(`coupon_gv_customer`)|(`coupon_redeem_track`)|(`coupons`)|(`coupons_description`)|(`customers_points`)|(`newsletters`)|(`orders_products_download`)|(`point_histories`)|(`products_with_attributes_stock`)|(`sessions`)/", $line)){
 				$def .= $line."\n";
 			}
 		}else{
