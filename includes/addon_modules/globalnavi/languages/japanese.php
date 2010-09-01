b5d025607451655bc2f849fa56ac3d42afaa3c3e
saito <saito>
2010-09-01 10:16:36 +0900

    言語ファイル修正

--- includes/addon_modules/globalnavi/languages/japanese.php
+++ includes/addon_modules/globalnavi/languages/japanese.php
@@ -1,27 +1,4 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2009 Liquid System Technology, Inc.                    |
-// | Author Koji Sasaki                                                   |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 The zen-cart developers                  |
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: japanese.php $
-//
-
   // 管理画面用
 define('MODULE_GLOBALNAVI_TITLE', 'グローバルナビ');
 define('MODULE_GLOBALNAVI_DESCRIPTION', 'グローバルナビ<br />グローバルナビを表示するブロックを追加します。<br />有効化後に<a href="' . zen_href_link(FILENAME_BLOCKS) . '">ブロックの設定</a>から表示設定をしてください。');
