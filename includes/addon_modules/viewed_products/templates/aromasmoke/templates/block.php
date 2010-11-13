ce7af773ad2309d10e26e7b55696583098b31d55
saito <saito>
2010-11-14 08:05:57 +0900

    いらないテンプレートを削除

--- includes/addon_modules/viewed_products/templates/aromasmoke/templates/block.php
+++
@@ -1,9 +0,0 @@
-<h2><?php echo MODULE_VIEWED_PRODUCTS_BLOCK_TITLE;?></h2>
-<?php foreach ($viewed_products as $viewed_product): ?>
-<dl>
-<dt><a href="<?php echo $viewed_product['url']?>"><?php echo zen_image($viewed_product['image'], $viewed_product['name'], $small_width, $small_height)?></a></dt>
-<dd class="name"><a href="<?php echo $viewed_product['url']?>"><?php echo $viewed_product['name']; ?></a></dd>
-<dd class="price"><?php echo $viewed_product['display_price']; ?></dd>
-</dl>
-<?php endforeach; ?>
-
