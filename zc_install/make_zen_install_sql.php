a61b5e26cd2060fbe3be9457b27b546d2592e172
saito <saito>
2010-12-14 12:43:19 +0900

    リストを見やすく修正

--- zc_install/make_zen_install_sql.php
+++ zc_install/make_zen_install_sql.php
@@ -4,17 +4,78 @@
 	$demo = "";
 	$def = "";
 
+	$demos  = array('(`categories`)',
+			'(`categories_description`)',
+			'(`ezpages`)',
+			'(`featured`)',
+			'(`group_pricing`)',
+			'(`manufacturers`)',
+			'(`manufacturers_info`)',
+			'(`media_clips`)',
+			'(`media_manager`)',
+			'(`media_to_products`)',
+			'(`meta_tags_categories_description`)',
+			'(`meta_tags_products_description`)',
+			'(`music_genre`)',
+			'(`product_music_extra`)',
+			'(`product_types_to_category`)',
+			'(`products`)',
+			'(`products_attributes`)',
+			'(`products_attributes_download`)',
+			'(`products_description`)',
+			'(`products_discount_quantity`)',
+			'(`products_options`)',
+			'(`products_options_values`)',
+			'(`products_options_values_to_products_options`)',
+			'(`products_to_categories`)',
+			'(`record_artists`)',
+			'(`record_artists_info`)',
+			'(`record_company`)',
+			'(`record_company_info`)',
+			'(`reviews`)',
+			'(`reviews_description`)',
+			'(`salemaker_sales`)',
+			'(`specials`)'
+			);
+	$demos_pattern = '/'.implode('|', $demos).'/';
+
+	$default = array('(`orders`)',
+			 '(`orders_products`)',
+			 '(`orders_products_attributes`)',
+			 '(`orders_total`)',
+			 '(`orders_status_history`)',
+			 '(`customers`)',
+			 '(`customers_info`)',
+			 '(`customers_basket`)',
+			 '(`address_book`)',
+			 '(`admin_activity_log`)',
+			 '(`banners_history`)',
+			 '(`counter_history`)',
+			 '(`coupon_email_track`)',
+			 '(`coupon_gv_customer`)',
+			 '(`coupon_redeem_track`)',
+			 '(`coupons`)',
+			 '(`coupons_description`)',
+			 '(`customers_points`)',
+			 '(`newsletters`)',
+			 '(`orders_products_download`)',
+			 '(`point_histories`)',
+			 '(`products_with_attributes_stock`)',
+			 '(`sessions`)'
+			 );
+	$default_pattern = '/'.implode('|', $default).'/';
+
 	foreach($lines as $line) {
 		$line = trim($line);
 		$line_upper = strtoupper($line);
 
 		if(substr($line_upper, 0, 12) == 'INSERT INTO '){
 			//デモ用データ
-			if(preg_match("/(`categories`)|(`categories_description`)|(`ezpages`)|(`featured`)|(`group_pricing`)|(`manufacturers`)|(`manufacturers_info`)|(`media_clips`)|(`media_manager`)|(`media_to_products`)|(`meta_tags_categories_description`)|(`meta_tags_products_description`)|(`music_genre`)|(`product_music_extra`)|(`product_types_to_category`)|(`products`)|(`products_attributes`)|(`products_attributes_download`)|(`products_description`)|(`products_discount_quantity`)|(`products_options`)|(`products_options_values`)|(`products_options_values_to_products_options`)|(`products_to_categories`)|(`record_artists`)|(`record_artists_info`)|(`record_company`)|(`record_company_info`)|(`reviews`)|(`reviews_description`)|(`salemaker_sales`)|(`specials`)/", $line)) {
+			if(preg_match($demos_pattern, $line)) {
 				$demo .= $line."\n";
 
 			//デフォルトデータ
-			}elseif(!preg_match("/(`orders`)|(`orders_products`)|(`orders_products_attributes`)|(`orders_total`)|(`orders_status_history`)|(`customers`)|(`customers_info`)|(`customers_basket`)|(`address_book`)|(`admin_activity_log`)|(`banners_history`)|(`counter_history`)|(`coupon_email_track`)|(`coupon_gv_customer`)|(`coupon_redeem_track`)|(`coupons`)|(`coupons_description`)|(`customers_points`)|(`newsletters`)|(`orders_products_download`)|(`point_histories`)|(`products_with_attributes_stock`)|(`sessions`)/", $line)){
+			}elseif(!preg_match($default_pattern, $line)){
 				$def .= $line."\n";
 			}
 		}else{
