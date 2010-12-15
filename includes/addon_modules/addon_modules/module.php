4cc8d2a35d70c916ef601c7ae5b34d25aeeac814
saito <saito>
2010-12-15 11:38:44 +0900

    #11535 ライセンスの確認をする

--- includes/addon_modules/addon_modules/module.php
+++ includes/addon_modules/addon_modules/module.php
@@ -3,8 +3,6 @@
  * addon_modules Core Module
  *
  * @package addon_modules
- * @copyright Copyright (C) 2009 Liquid System Technology, Inc.
- * @author Koji Sasaki
  * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
@@ -20,6 +18,11 @@ if (!defined('IS_ADMIN_FLAG')) {
    *
    */
   class addon_modules extends addOnModuleBase {
+    var $author = 'Koji Sasaki';
+    var $author_email = 'info@zencart-sugu.jp';
+    var $require_zen_cart_version = '1.3.0.2';
+    var $require_addon_modules_version = '1.0.0';
+
     /**
      * Display modules title on admin.
      * @var string
