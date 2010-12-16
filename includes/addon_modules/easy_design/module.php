440eafe911a46e0a525a686d65db6e65c52cbefd
saito <saito>
2010-12-16 19:18:36 +0900

    #11535 ライセンスの確認をする

--- includes/addon_modules/easy_design/module.php
+++ includes/addon_modules/easy_design/module.php
@@ -3,12 +3,9 @@
  * addon_modules_example Module
  *
  * @package Viewed_products
- * @copyright Copyright (C) 2009 Liquid System Technology, Inc.
- * @author Koji Sasaki
  * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @author Koji Sasaki <sasaki@liquidst.jp>
  * @version $Id: addon_modules_example.php $
  */
 if (!defined('IS_ADMIN_FLAG')) {
@@ -44,7 +41,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     var $require_modules = array();
     var $notifier        = array();
 
-    var $author                        = "kohata";
+    var $author                        = array("kohata");
     var $author_email                  = "info@zencart-sugu.jp";
     var $version                       = "0.1";
     var $require_zen_cart_version      = "1.3.0.2";
