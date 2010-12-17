b18f8bcf22963a32722680527c0513dccd670133
saito <saito>
2010-12-17 19:47:31 +0900

    #11535 ライセンスの確認をする

--- includes/addon_modules/zen_tweet/database_tables.php
+++ includes/addon_modules/zen_tweet/database_tables.php
@@ -3,8 +3,6 @@
  * addon_modules_skeleten - Database Name Defines
  *
  * @package classes
- * @copyright Copyright 2009 Liquid System Technology, Inc.
- * @author Koji Sasaki
  * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
