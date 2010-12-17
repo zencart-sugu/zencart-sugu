d419f95a947ff80e5009c6b7abbf0fefae622142
saito <saito>
2010-12-17 10:13:29 +0900

    #11535 ライセンスの確認をする

--- includes/addon_modules/jquery/module.php
+++ includes/addon_modules/jquery/module.php
@@ -3,8 +3,6 @@
  * jquery Module
  *
  * @package Addon Modules
- * @copyright Copyright (C) 2009 Liquid System Technology, Inc.
- * @author Koji Sasaki
  * @copyright Portions Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
@@ -58,7 +56,8 @@ if (!defined('IS_ADMIN_FLAG')) {
     var $require_modules = array();
     var $notifier = array();
 
-    var $author                        = "Koji Sasaki";
+    var $author                        = array("Koji Sasaki",
+                                               "The jQuery Project");
     var $author_email                  = "info@zencart-sugu.jp";
     var $version                       = "0.1";
     var $require_zen_cart_version      = "1.3.0.2";
