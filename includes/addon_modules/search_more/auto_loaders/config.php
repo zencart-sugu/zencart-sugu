cd0cf2d1c3c055ba61add375238004037513d8f8
saito <saito>
2010-12-24 14:53:46 +0900

    #13866 検索結果全般
    表示件数変更に対応

---
+++ includes/addon_modules/search_more/auto_loaders/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$autoLoadConfig[35][] = array('autoType' => 'require',
+                              'loadFile' => DIR_FS_CATALOG_ADDON_MODULES . 'search_more/auto_loaders/init.php');
+
+?>
