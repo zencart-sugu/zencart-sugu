951986525a109ca189cddd4e68f92adbb179af4f
saito <saito>
2011-01-05 09:31:11 +0900

    #13864 微調整

--- includes/addon_modules/carousel_ui/templates/template_default/jscript/jscript_carousel_ui.js
+++ includes/addon_modules/carousel_ui/templates/template_default/jscript/jscript_carousel_ui.js
@@ -1,23 +1,25 @@
-jQuery(document).ready(function() {
-  var els = jQuery('.block-carousel_ui dl');
-  var i = 0;
-  var j = els.size();
-  for (; i<j; i++) {
-    var el = els.eq(i);
-    el.mouseover(function(ev){
-      var t = ev.target;
-      while(t.tagName != 'DL') {
-        t = t.parentNode;
-      }
-      t.backup = t.style.backgroundColor;
-      t.style.backgroundColor = '#E1EBF5';
-    });
-    el.mouseout(function(ev){
-      var t = ev.target;
-      while(t.tagName != 'DL') {
-        t = t.parentNode;
-      }
-      t.style.backgroundColor = t.backup;
-    });
-  }
-});
+if (jQuery) {
+  jQuery(document).ready(function() {
+    var els = jQuery('.block-carousel_ui dl');
+    var i = 0;
+    var j = els.size();
+    for (; i<j; i++) {
+      var el = els.eq(i);
+      el.mouseover(function(ev){
+        var t = ev.target;
+        while(t.tagName != 'DL') {
+          t = t.parentNode;
+        }
+        t.backup = t.style.backgroundColor;
+        t.style.backgroundColor = '#E1EBF5';
+      });
+      el.mouseout(function(ev){
+        var t = ev.target;
+        while(t.tagName != 'DL') {
+          t = t.parentNode;
+        }
+        t.style.backgroundColor = t.backup;
+      });
+    }
+  });
+}
