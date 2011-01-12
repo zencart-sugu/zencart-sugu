709236eed85cc048851aa129b46ad96e4fdc4f29
Otsuji Takashi <o2gtkc@gmail.com>
2011-01-12 19:04:33 +0900

    ・english用のファイルの追加

---
+++ includes/addon_modules/ajax_category_tree/languages/english.php
@@ -0,0 +1,38 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2008 Liquid System Technology, Inc.                    |
+// | Author Koji Sasaki                                                   |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 The zen-cart developers                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: japanese.php $
+//
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_TITLE',                  '商品カテゴリー');
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_DESCRIPTION',            'カテゴリーをツリー表示にします');
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_STATUS_TITLE',           '商品カテゴリーの有効化');
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_STATUS_DESCRIPTION',     '商品カテゴリー表示を有効にしますか？ <br />true: 有効<br />false: 無効');
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_SORT_ORDER_TITLE',       '優先順');
+define('MODULE_ADDON_MODULES_AJAXCATEGORYTREE_SORT_ORDER_DESCRIPTION', 'モジュールの優先順を設定できます。数字が小さいほど先にモジュールの読み込みと処理が実行されます。半角数字で他のモジュールと重ならないように設定してください。');
+
+define('MODULE_AJAX_CATEGORY_TREE_TITLE',          '商品カテゴリー');
+define('MODULE_AJAX_CATEGORY_TREE_BLOCK_TITLE',    '商品カテゴリー');
+define('MODULE_ADDON_MODULES_TEXT_ALL_CATEGORIES', '全カテゴリー');
+
+// header_navigation extra_boxes
+define('BOX_AJAXCATEGORYTREE',               'Product Categories');
+define('MODULE_AJAXCATEGORYTREE_PAGE_TITLE', 'Product Categories');
+?>
