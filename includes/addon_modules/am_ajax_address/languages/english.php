26e4952d3b57926b8eb6ccb015b238afd32be350
Otsuji Takashi <o2gtkc@gmail.com>
2011-01-12 19:25:42 +0900

    ・english用のファイルの追加

---
+++ includes/addon_modules/am_ajax_address/languages/english.php
@@ -0,0 +1,29 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2009 Liquid System Technology, Inc.                    |
+// | Author Koji Sasaki                                                   |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 The zen-cart developers                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: japanese.php $
+//
+define('MODULE_AM_AJAX_ADDRESS_TITLE',             '郵便番号による住所自動入力');
+define('MODULE_AM_AJAX_ADDRESS_DESCRIPTION',       '郵便番号による住所自動入力');
+
+define('MODULE_AM_AJAX_ADDRESS_STATUS_TITLE',             '郵便番号による住所自動入力の有効化');
+define('MODULE_AM_AJAX_ADDRESS_STATUS_DESCRIPTION',       '郵便番号による住所自動入力を有効にしますか？ <br />true: 有効<br />false: 無効');
+?>
