02100869faf7f5496ce823e99cca76cdc4e67a2c
Otsuji Takashi <o2gtkc@gmail.com>
2011-01-14 14:13:13 +0900

    ・english用のファイルの追加

--- includes/addon_modules/email_templates/languages/english.php
+++ includes/addon_modules/email_templates/languages/english.php
@@ -17,118 +17,150 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: english.php $
+// $Id: japanese.php $
 //
-define('MODULE_EMAIL_TEMPLATE_CREATE_LANGUAGE_ID_EN', '1');
+define('MODULE_EMAIL_TEMPLATES_TITLE', 'メールテンプレート');
+define('MODULE_EMAIL_TEMPLATES_DESCRIPTION', 'メールテンプレート機能を提供します。');
+define('MODULE_EMAIL_TEMPLATES_STATUS_TITLE', 'メールテンプレートの有効化');
+define('MODULE_EMAIL_TEMPLATES_STATUS_DESCRIPTION', 'メールテンプレートを有効にしますか？ <br />true: 有効<br />false: 無効');
+define('MODULE_EMAIL_TEMPLATES_SORT_ORDER_TITLE', '優先順');
+define('MODULE_EMAIL_TEMPLATES_SORT_ORDER_DESCRIPTION', 'モジュールの優先順を設定できます。数字が小さいほど先にモジュールの読み込みと処理が実行されます。半角数字で他のモジュールと重ならないように設定してください。');
 
-//define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_ID_EN', '1');
-define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_SUBJECT_EN', 'Thank you for membership registration');
-define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_BODY_EN', 'Thank you for membership registration');
+define('BOX_TOOLS_EMAIL_TEMPLATES', 'Emailテンプレート');
+define('TEXT_EMAIL_TEMPLATE', 'Emailテンプレート: ');
+define('TEXT_EMAIL_TEMPLATE_SETUP_PAGE', 'セットアップページ');
+define('TEXT_EMAIL_TEMPLATE_EMPTY', 'Emailテンプレートがありません');
+define('TEXT_EMAIL_TEMPLATE_DESCRIPTION', 'コメント:'.BOX_TOOLS_EMAIL_TEMPLATES.'内の[COMMENTS]に埋め込まれます<br />(テンプレート内に[COMMENTS]が無い場合はコメントの埋め込みは行われません)');
+define('TEXT_EMAIL_TEMPLATE_NO_TEMPLATE', BOX_TOOLS_EMAIL_TEMPLATES.'が見つからなかったためメールの送信は行われませんでした。');
 
-//define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_ID_EN', '2');
-define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_SUBJECT_EN', 'Thank you for the order[member]');
-define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_BODY_EN', 'OrderConfirmation from XXXXXXXX
+define('MODULE_EMAIL_TEMPLATE_CREATE_LANGUAGE_ID', '2');
 
-[CUSTOMER_NAME]
+define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_GRP', 'User registration');
+define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_TITLE', 'Thank you for registration');
+define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_SUBJECT', 'Thank you for registration');
+define('MODULE_EMAIL_TEMPLATE_CREATE_ACCOUNT_MAIL_BODY', 'Thank you for registration.
 
-OrderNumber: [ORDER_ID]
-OrderDate: [DATE_ORDERED]
+Please enjoy shopping.');
+
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_GRP', 'Order is complete.');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_TITLE', 'Thank you for your order.[For Members]');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_SUBJECT', 'Thank you for your order.[For Members]');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_MAIL_BODY', 'Order Confirmation from XXXXXXXX
+
+Dear [CUSTOMER_NAME]
+
+Thank you for your order.
+Your order is as follows.
+------------------------------------------------------
+Order Number: [ORDER_ID]
+Order Date: [DATE_ORDERED]
 Invoice:
 [INVOICE_URL]
 
 [COMMENT]
 
-Products
+Products Ordered
 ------------------------------------------------------
 [PRODUCTS_ORDERED]
 ------------------------------------------------------
 [TOTALS]
 
-Shipping
+Delivery Address
 ------------------------------------------------------
 [DELIVERY_ADDRESS]
 
-InvoiceAddress
+Billing Address
 ------------------------------------------------------
 [BILLING_ADDRESS]
 
-Payment
+Payment Method
 ------------------------------------------------------
 [PAYMENT_METHOD]
 
+Thank you.
 
 -----
-This E-mail is sent to the customer registered in this shop.
-Very sorry to trouble you, but with mail when there is no mind hit
-xxxxxxx@example.org
-
+This email sent to customers who register to our shopping site.
+If you do not remember this email,please contact us at xxxxxxx@example.org.
 -----
 Copyright (c) XXXXXXXX Inc. All Rights Reserved
 ');
 
-//define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_ID_EN', '3');
-define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_SUBJECT_EN', 'Thank you for the order[guest]');
-define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_BODY_EN', 'OrderConfirmation from XXXXXXXX
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_GRP', 'Order is complete.');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_TITLE', 'Thank you for your order.[For Guests]');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_SUBJECT', 'Thank you for your order.[For Guests]');
+define('MODULE_EMAIL_TEMPLATE_CHECKOUT_SUCCESS_VISITOR_MAIL_BODY', 'Order Confirmation from XXXXXXXX
 
-[CUSTOMER_NAME]
+Dear [CUSTOMER_NAME]
 
-OrderNumber: [ORDER_ID]
-OrderDate: [DATE_ORDERED]
-Invoice:
-[INVOICE_URL]
+Thank you for your order.
+Your order is as follows.
+------------------------------------------------------
+Order Number: [ORDER_ID]
+Order Date: [DATE_ORDERED]
 
 [COMMENT]
 
-Products
+Products Ordered
 ------------------------------------------------------
 [PRODUCTS_ORDERED]
 ------------------------------------------------------
 [TOTALS]
 
-Shipping
+Delivery Address
 ------------------------------------------------------
 [DELIVERY_ADDRESS]
 
-InvoiceAddress
+Billing Address
 ------------------------------------------------------
 [BILLING_ADDRESS]
 
-Payment
+Payment Method
 ------------------------------------------------------
 [PAYMENT_METHOD]
 
+Thank you.
 
 -----
-This E-mail is sent to the customer registered in this shop.
-Very sorry to trouble you, but with mail when there is no mind hit
-xxxxxxx@example.org
-
+This email sent to customers who register to our shopping site.
+If you do not remember this email,please contact us at xxxxxxx@example.org.
 -----
 Copyright (c) XXXXXXXX Inc. All Rights Reserved
 ');
 
-//define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_ID_EN', '4');
-define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_SUBJECT_EN', 'Information of order situation');
-define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_BODY_EN', '
-[CUSTOMER_NAME]
+define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_GRP',     'User notification');
+define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_TITLE',   'Status Change');
+define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_SUBJECT', 'Notice of order receipt status');
+define('MODULE_EMAIL_TEMPLATE_STATUS_MAIL_BODY', '
+Dear [CUSTOMER_NAME]
 
-Thank you for use
-[DATE_ORDERED]
-Order receipt number：[ORDER_ID]
+Thank you for your order.
+To announce the status of Order Number:[ORDER_ID] ,your order on [DATE_ORDERED].
 
 [INVOICE_URL]
 
 [COMMENTS]
 
------
-This E-mail is sent to the customer registered in this shop.
-Very sorry to trouble you, but with mail when there is no mind hit
-xxxxxxx@example.org
+Thank you.
 
+-----
+This email sent to customers who register to our shopping site.
+If you do not remember this email,please contact us at xxxxxxx@example.org.
 -----
 Copyright (c) XXXXXXXX Inc. All Rights Reserved
 ');
 
-define('MODULE_EMAIL_TEMPLATE_NOT_DELIVERY', 'NONE');
-define('MODULE_EMAIL_TEMPLATE_INVOICE_TEXT', 'You can see ordering information');
+define('MODULE_EMAIL_TEMPLATE_STATUS_CHANGE_NO_NOTIFY', 'No');
+define('MODULE_EMAIL_TEMPLATE_DATE_FORMAT_LONG',        '%Y/%m/%d ');
+
+define('MODULE_EMAIL_TEMPLATE_SUN', 'SUN');
+define('MODULE_EMAIL_TEMPLATE_MON', 'MON');
+define('MODULE_EMAIL_TEMPLATE_TUE', 'TUE');
+define('MODULE_EMAIL_TEMPLATE_WED', 'WED');
+define('MODULE_EMAIL_TEMPLATE_THU', 'THU');
+define('MODULE_EMAIL_TEMPLATE_FRI', 'FRI');
+define('MODULE_EMAIL_TEMPLATE_SAT', 'SAT');
+
+define('MODULE_EMAIL_TEMPLATE_NOT_DELIVERY', 'Non');
+define('MODULE_EMAIL_TEMPLATE_INVOSICE_TEXT', 'ご注文についての情報は下記URLでご覧いただけます。');
 ?>
