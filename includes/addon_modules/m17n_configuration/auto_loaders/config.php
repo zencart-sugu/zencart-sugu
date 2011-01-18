3b02c90f8c7029d506dc4654f12f58a37df0cd36
saito <saito>
2011-01-18 15:20:38 +0900

    #12420
    zen_mobile、zen_smartphoneをm17n_configurationモジュールを使うように変更したい
    auto_loaderを2回に分割 & 関数の移動
    use_function用関数追加

--- includes/addon_modules/m17n_configuration/auto_loaders/config.php
+++ includes/addon_modules/m17n_configuration/auto_loaders/config.php
@@ -2,5 +2,6 @@
 
 $autoLoadConfig[15][] = array('autoType'=>'require',
                               'loadFile'=> DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/auto_loaders/init.php');
-
+$autoLoadConfig[75][] = array('autoType'=>'require',
+                              'loadFile'=> DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/auto_loaders/init2.php');
 ?>
---
+++ includes/addon_modules/m17n_configuration/auto_loaders/init2.php
@@ -0,0 +1,8 @@
+<?php
+require_once DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/configure.php';
+require_once DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/database_tables.php';
+require_once DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/functions.php';
+require_once DIR_FS_CATALOG_ADDON_MODULES . 'm17n_configuration/auto_loaders/init_functions.php';
+
+zen_m17n_init_db_config_read();
+?>
