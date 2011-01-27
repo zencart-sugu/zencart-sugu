59cc37cdb5a929526421feda4ce54f2657fd0dcc
Otsuji Takashi <o2gtkc@gmail.com>
2011-01-27 11:03:46 +0900

    ・english用のファイルの追加

---
+++ includes/addon_modules/zen_smartphone/languages/english.php
@@ -0,0 +1,30 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 The zen-cart developers                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: japanese.php $
+//
+define('MODULE_ZEN_SMARTPHONE_TITLE',                  'zen スマートフォン');
+define('MODULE_ZEN_SMARTPHONE_DESCRIPTION',            'zen スマートフォン');
+
+define('MODULE_ZEN_SMARTPHONE_STATUS_TITLE',           'zen スマートフォンの有効化');
+define('MODULE_ZEN_SMARTPHONE_STATUS_DESCRIPTION',     'zen スマートフォンを有効にしますか？ <br />true: 有効<br />false: 無効');
+
+define('MODULE_ZEN_SMARTPHONE_SORT_ORDER_TITLE',       '優先順');
+define('MODULE_ZEN_SMARTPHONE_SORT_ORDER_DESCRIPTION', 'モジュールの優先順を設定できます。数字が小さいほど先にモジュールの読み込みと処理が実行されます。半角数字で他のモジュールと重ならないように設定してください。');
+?>
