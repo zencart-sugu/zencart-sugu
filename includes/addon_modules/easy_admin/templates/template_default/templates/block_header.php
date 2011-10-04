7423c5f31b08fa2553e6817f8c795227153d0311
ohmura <ohmura@ark-web.jp>
2011-10-04 13:53:47 +0900

    refs #17581 easy_adminでヘッダを置換する箇所を変更

--- includes/addon_modules/easy_admin/templates/template_default/templates/block_header.php
+++ includes/addon_modules/easy_admin/templates/template_default/templates/block_header.php
@@ -1,27 +1,3 @@
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-JP">
-<title><?php echo TITLE; ?></title>
-<script language="JavaScript" src="includes/menu.js" type="text/JavaScript"></script>
-<link href="includes/stylesheet.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" href="includes/cssjsmenuhover.css" media="all" id="hoverJS" />
-<script type="text/javascript">
-  <!--
-  function init()
-  {
-    cssjsmenu('navbar');
-    if (document.getElementById)
-    {
-      var kill = document.getElementById('hoverJS');
-      kill.disabled = true;
-    }
-  }
-  // -->
-</script>
-</head>
-<body onload="init()">
-
 <!-- bof languages dropdown -->
 <?php echo $GLOBALS['easy_admin']->getBlock('block_languages_dropdown', $current_page_base); ?>
 <!-- eof languages dropdown -->
