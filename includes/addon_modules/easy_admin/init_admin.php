d2e0464654c80cb113e98298bb8f366fe6ff4094
ohmura <ohmura@ark-web.jp>
2011-11-07 07:35:25 +0900

    refs #17587 easy_admin: htmlヘッダにCSS追加

--- includes/addon_modules/easy_admin/init_admin.php
+++ includes/addon_modules/easy_admin/init_admin.php
@@ -3,6 +3,7 @@
 
   if(basename($_SERVER['PHP_SELF']) != FILENAME_LOGIN . '.php') {
     $GLOBALS['easy_admin_block_header'] = $GLOBALS['easy_admin']->getBlock('block_header', $current_page_base);
+    $GLOBALS['easy_admin_block_append_html_header'] = $GLOBALS['easy_admin']->getBlock('block_append_html_header', $current_page_base);
     ob_start("handle_easy_admin_ob");
   }
 ?>
