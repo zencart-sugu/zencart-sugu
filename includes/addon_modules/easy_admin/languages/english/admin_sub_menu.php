dfacb5a1c26be28e4a32cc8a34b461af2ad73277
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 13:04:03 +0900

    refs #14116 addon_modulesの英語用文言を翻訳する 英語ファイルを翻訳したものに差し替え

---
+++ includes/addon_modules/easy_admin/languages/english/admin_sub_menu.php
@@ -0,0 +1,52 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+//  $Id: cache.php 1105 2005-04-04 22:05:35Z birdbrain $
+//
+
+define('HEADING_TITLE',                      'Management Sub Menu Setting');
+
+define('TEXT_HEADER_SUB_MENU',               ' Sub Menu List');
+define('TEXT_HEADER_SUB_MENU_NAME',          'Menu Name');
+define('TEXT_HEADER_SUB_MENU_OLD',           'Corresponding Old Menu');
+define('TEXT_HEADER_SUB_MENU_ORDER',         'Sort Order');
+define('TEXT_HEADER_SUB_MENU_ACTION',        'Action');
+
+define('TEXT_MENU_OLD_UNDEFINE',             '[No Corresponding Old Menu]');
+
+define('TEXT_ACTION_BACK',                   'Back');
+define('TEXT_ACTION_ADD',                    'Add');
+define('TEXT_ACTION_EDIT',                   'Edit');
+define('TEXT_ACTION_DELETE',                 'Delete');
+define('TEXT_ACTION_UPDATE',                 'Update');
+define('TEXT_ACTION_SUBMENU',                'Management Sub Menu');
+
+define('TEXT_INPUT_ADD',                     '[Add]');
+define('TEXT_INPUT_NAME',                    'Menu Name');
+define('TEXT_INPUT_OLD',                     'Corresponding Old Menu');
+define('TEXT_INPUT_ORDER',                   'Sort Order');
+
+define('TEXT_ERROR_MENU_NAME',               'Please input the menu name.');
+define('TEXT_ERROR_MENU_SELECT',             'Please select a correspondence old menu.');
+define('TEXT_ERROR_MENU_ORDER',              'Please input the sort order.');
+define('TEXT_INSERT_SUCCESS_SUBMENU',        'Sub menu was added.');
+define('TEXT_UPDATE_SUCCESS_SUBMENU',        'Sub menu was updated');
+define('TEXT_DELETE_SUCCESS_SUBMENU',        'Sub menu was deleted');
+define('TEXT_CONFIRM_MENU_DELETE',           ' Do you want to delete this?');
+?>
