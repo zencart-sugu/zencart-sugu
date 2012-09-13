036ee29f63a01fe7b08862328b2ab2aafe7c98e7
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 14:08:23 +0900

    refs #14116 addon_modulesの英語用文言を翻訳する 英語ファイルを翻訳したものに差し替え

---
+++ includes/addon_modules/email_templates/languages/addon_modules/english.php
@@ -0,0 +1,38 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 The zen-cart developers                  |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: japanese.php $
+//
+define('MODULE_ADDON_MODULES_EXAMPLE_TITLE', 'Addon Modules Example');
+define('MODULE_ADDON_MODULES_EXAMPLE_DESCRIPTION', 'Addon Modules Example');
+define('MODULE_ADDON_MODULES_EXAMPLE_STATUS_TITLE', 'Activating Addon Modules Example');
+define('MODULE_ADDON_MODULES_EXAMPLE_STATUS_DESCRIPTION', 'Do you want to active to Addon Modules Example? <br />true: Active<br />false: Inactive');
+define('MODULE_ADDON_MODULES_EXAMPLE_SORT_ORDER_TITLE', 'Sort Order');
+define('MODULE_ADDON_MODULES_EXAMPLE_SORT_ORDER_DESCRIPTION', 'I can set the priority order of the module. Reading and the disposal of modules are carried out earlier so that a number is small. Please set it not to fall on other modules with a half size number.');
+
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_TITLE', 'Overridden of Example Block Title');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_VAR_1', 'Appropriate content 1');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_VAR_2', 'Appropriate content 2');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_VAR_3', 'Appropriate content 3');
+
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_HOGEHOGE_TITLE', 'Example Block hogegoe');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_HOGEHOGE_LIST_1', 'Appropriate list 1');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_HOGEHOGE_LIST_2', 'Appropriate list 2');
+define('MODULE_ADDON_MODULES_EXAMPLE_BLOCK_HOGEHOGE_LIST_3', 'Appropriate list 3');
+?>
