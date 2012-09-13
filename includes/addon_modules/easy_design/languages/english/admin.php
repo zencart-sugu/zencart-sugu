ae2780f3fded07b6fb0c770ff5ae3fb8d2227fc7
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 13:31:44 +0900

    refs #14116 忘れもの

---
+++ includes/addon_modules/easy_design/languages/english/admin.php
@@ -0,0 +1,46 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+//  $Id: cache.php 1105 2005-04-04 22:05:35Z birdbrain $
+//
+
+define('HEADING_TITLE',                      'Design Setting');
+
+define('TEXT_HEADER_CHANGE_TEMPLATE',        'Chang Template');
+define('TEXT_HEADER_CHANGE_COLOR',           'Chang Color');
+define('TEXT_HEADER_CHANGE_OBJECTION',       'Chang Wording');
+define('TEXT_HEADER_CHANGE_LOGO',            'Chang Logo Image');
+
+define('TEXT_ACTION_CHANGE',                 'Change');
+define('TEXT_ACTION_SAVE',                   'Save');
+define('TEXT_ACTION_RECOVERY',               'Restore Defaults');
+
+define('TEXT_UPDATE_SUCCESS_TEMPLATE',       'Template Changed');
+define('TEXT_UPDATE_SUCCESS_COLOR',          'Color changed');
+define('TEXT_UPDATE_FAILURE_COLOR',          'Failed to change the color¡ÊCan not write css file¡Ë');
+define('TEXT_UPDATE_SUCCESS_OBJECTION',      'Wording Changed');
+define('TEXT_UPDATE_SUCCESS_LOGO',           'Changed Logo Image');
+define('TEXT_UPDATE_FAILURE_LOGO',           'Failed to change the logo image');
+define('TEXT_UPDATE_FAILURE_LOGO_EXT',       'You can upload only images (gif,jpg,bmp,tif,png)');
+define('TEXT_UPDATE_FAILURE_LOGO_UNLINK',    'Not delete existing file');
+define('TEXT_UPDATE_FAILURE_LOGO_DIR',       'Images/logo directory does not exist in the template');
+define('TEXT_UPDATE_FAILURE_LOGO_PERMIT',    'Images/logo directory in the template is not writable');
+
+define('TEXT_CONFIRM_COLOR_RECOVERY',        'Can I return to initial state color?');
+?>
