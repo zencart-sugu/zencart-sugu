12ca987184e58d2debebbb922f2c6ca009b11d00
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 14:17:58 +0900

    refs #14116 addon_modulesの英語用文言を翻訳する 英語ファイルを翻訳したものに差し替え

---
+++ includes/addon_modules/feature_area/languages/english/admin.php
@@ -0,0 +1,64 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2003 The zen-cart developers                           |
+// |                                                                      |
+// | http://www.zen-cart.com/index.php                                    |
+// |                                                                      |
+// | Portions Copyright (c) 2003 osCommerce                               |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+//  $Id: cache.php 1105 2005-04-04 22:05:35Z birdbrain $
+//
+
+define('HEADING_TITLE',                     'Feature Area UI');
+define('TABLE_HEADING_ID',                  'ID');
+define('TABLE_HEADING_ACTION',              'Action');
+define('TABLE_HEADING_STATUS',              'Status');
+define('TABLE_HEADING_SORT_ORDER',          'Sort Order');
+define('TABLE_HEADING_FEATURE_NAME',        'Feature');
+define('TABLE_HEADING_FEATURE_MAIN_IMAGE',  'Image of Main Area');
+define('TABLE_HEADING_FEATURE_THUMB_IMAGE', 'Thumbnail Image');
+define('TABLE_HEADING_FEATURE_LINK_URL',    'Link');
+define('TEXT_DATE_ADDED',                   'Creation Date');
+define('TEXT_LAST_MODIFIED',                'Last Update');
+define('TEXT_HEADING_NEW_FEATURE',          'New Feature');
+define('TEXT_NEW_INTRO',                    'Please enter a description of the feature.');
+define('TEXT_FEATURE_NAME',                 'Feature');
+define('TEXT_FEATURE_URL',                  'Link');
+define('TEXT_FEATURE_MAIN_IMAGE',           'Main Feature Image');
+define('TEXT_FEATURE_THUMB_IMAGE',          'Features Thumbnail Image');
+define('TEXT_FEATURE_MAIN_IMAGE_DIR',       'Main Image Directory');
+define('TEXT_FEATURE_THUMB_IMAGE_DIR',      'Thumbnails Directory');
+define('TEXT_FEATURE_STATUS',               'Show Features');
+define('TEXT_FEATURE_STATUS_TRUE',          'Display');
+define('TEXT_FEATURE_STATUS_FALSE',         'No Display');
+define('TEXT_FEATURE_NEW_WINDOW',           'Features open new window');
+define('TEXT_FEATURE_NEW_WINDOW_TRUE',      'Yes');
+define('TEXT_FEATURE_NEW_WINDOW_FALSE',     'No');
+define('TEXT_FEATURE_SORT_ORDER',           'Sort Order');
+
+define('TEXT_HEADING_EDIT_FEATURE', 'Edit Features');
+define('TEXT_EDIT_INTRO', 'Please correct the contents of the Features.');
+define('TEXT_IMAGE_NONEXISTENT', 'No Image');
+
+define('TEXT_HEADING_DELETE_FEATURE', 'Delete Features');
+define('TEXT_DELETE_INTRO', 'Do you want to delete this Features?');
+define('TEXT_DELETE_IMAGE', 'Do you want to delete the image?');
+
+define('TEXT_DISPLAY_NUMBER_OF_FEATURES', ' Display  <strong>%d</strong> to <strong>%d</strong> ( all <strong>%d</strong> )');
+
+define('SUCCESS_FEATURE_AREA_STATUS_UPDATED', 'SUCCESS: Features status has changed.');
+define('ERROR_UNKNOWN_STATUS_FLAG', 'ERROR: The status is unknown.');
+
+
+?>
