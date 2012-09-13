adb6a7e0b815fbc16eb4404f5537d5a3c4282cea
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 12:37:46 +0900

    refs #14116 addon_modulesの英語用文言を翻訳する 英語ファイルを翻訳したものに差し替え

--- includes/addon_modules/category_sitemap/languages/english.php
+++ includes/addon_modules/category_sitemap/languages/english.php
@@ -21,17 +21,17 @@
 // +----------------------------------------------------------------------+
 // $Id: japanese.php $
 //
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TITLE',                  'カテゴリーサイトマップ');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_DESCRIPTION',            'カテゴリーサイトマップを表示します');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_STATUS_TITLE',           'カテゴリーサイトマップの有効化');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_STATUS_DESCRIPTION',     'カテゴリーサイトマップ表示を有効にしますか？ <br />true: 有効<br />false: 無効');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_TITLE',       '表示するカテゴリーの深さ');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_DESCRIPTION', '表示するカテゴリーの深さを指定します（デフォルト='.MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_DEFAULT.'）');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_SORT_ORDER_TITLE',       '優先順');
-define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_SORT_ORDER_DESCRIPTION', 'モジュールの優先順を設定できます。数字が小さいほど先にモジュールの読み込みと処理が実行されます。半角数字で他のモジュールと重ならないように設定してください。');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TITLE',                  'Category Site Map');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_DESCRIPTION',            'Display category site map');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_STATUS_TITLE',           'Activating Category Site Map');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_STATUS_DESCRIPTION',     'Do you want to active to category site map?<br />true: Active<br />false: Inactive');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_TITLE',       'Display The Category Hierarchy');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_DESCRIPTION', 'Specify to display the category hierarchy. (Default='.MODULE_ADDON_MODULES_CATEGORY_SITEMAP_TREE_LEVEL_DEFAULT.')');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_SORT_ORDER_TITLE',       'Sort Order');
+define('MODULE_ADDON_MODULES_CATEGORY_SITEMAP_SORT_ORDER_DESCRIPTION', 'I can set the priority order of the module. Reading and the disposal of modules are carried out earlier so that a number is small. Please set it not to fall on other modules with a half size number.');
 
-define('MODULE_CATEGORY_SITEMAP_TITLE',            'カテゴリーサイトマップ');
-define('MODULE_CATEGORY_SITEMAP_BLOCK_TITLE',      'カテゴリーサイトマップ');
+define('MODULE_CATEGORY_SITEMAP_TITLE',            'Category Site Map');
+define('MODULE_CATEGORY_SITEMAP_BLOCK_TITLE',      'Category Site Map');
 
 // header_navigation extra_boxes
 define('BOX_CATEGORY_SITEMAP',               'Products Categories List');
