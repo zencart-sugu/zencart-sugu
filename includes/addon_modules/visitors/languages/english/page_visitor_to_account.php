7eb839a6dfed381590e136ca74ec514bde8e105b
tiadeen2 <tiadeen2@yahoo.co.jp>
2012-09-13 17:12:07 +0900

    refs #14116 addon_modulesの英語用文言を翻訳する 英語ファイルを翻訳したものに差し替え

--- includes/addon_modules/visitors/languages/english/page_visitor_to_account.php
+++ includes/addon_modules/visitors/languages/english/page_visitor_to_account.php
@@ -8,9 +8,9 @@
  */
 
 
-define('NAVBAR_TITLE', 'アカウント作成');
+define('NAVBAR_TITLE', 'Create Account');
 
-define('HEADING_TITLE', 'アカウント作成');
+define('HEADING_TITLE', 'Create Account');
 
 define('TEXT_ORIGIN_LOGIN', '<strong class="note">Attention:</strong>If you already have Customer Account in our store, please login <a href="%s">here.</a>');
 
@@ -21,43 +21,39 @@ define('EMAIL_GREET_MS', 'Ms %s' . "\n\n");
 define('EMAIL_GREET_NONE', 'For %s' . "\n\n");
 
 // First line of the greeting
-define('EMAIL_WELCOME', '謹啓　この度は<strong>' . STORE_NAME . 'にご登録いただきありがとうございました。</strong>');
+define('EMAIL_WELCOME', 'Dear Sir/Madam, <strong>Thank you for registering in ' . STORE_NAME . ' for this time.</strong>');
 define('EMAIL_SEPARATOR', '--------------------');
-define('EMAIL_COUPON_INCENTIVE_HEADER', 'ご登録いただいたお礼に次回<strong>' . STORE_NAME . '</strong>をご利用の際にお使い' . "\n" .'いただける「割引クーポン」をお送りします!' . "\n\n");
+define('EMAIL_COUPON_INCENTIVE_HEADER', 'When the next <strong>' . STORE_NAME . '</strong> is used, "Discount Coupon" that can be used is sent to the registered reward.' . "\n\n");
 // your Discount Coupon Description will be inserted before this next define
-define('EMAIL_COUPON_REDEEM', 'クーポンコード： <strong>%s</strong>' . "\n\n" . 'この割引クーポンをお使いになるには、お買い物' . "\n" . 'の精算時に上記コードを入力してください。' . "\n\n");
+define('EMAIL_COUPON_REDEEM', 'Coupon Code： <strong>%s</strong>' . "\n\n" . 'Please input the above-mentioned code to use this discount coupon when you adjust shopping.' . "\n\n");
 
-define('EMAIL_GV_INCENTIVE_HEADER', '本日に限り %sの' . TEXT_GV_NAME . 'をお送りします!' . "\n");
-define('EMAIL_GV_REDEEM', 'The ' . TEXT_GV_NAME . ' ' . TEXT_GV_REDEEM . ' は: %s ' . "\n\n" . 'お客様が当ショップで商品をお選びになった後、精算時に「' . TEXT_GV_REDEEM . 'を入力していただくことでお使いいただけます。');
-define('EMAIL_GV_LINK', '下記のリンクから今すぐ引き換えることもできます。: ' . "\n");
+define('EMAIL_GV_INCENTIVE_HEADER', TEXT_GV_NAME . ' of %s is sent only today.' . "\n");
+define('EMAIL_GV_REDEEM', 'The ' . TEXT_GV_NAME . ' ' . TEXT_GV_REDEEM . ': %s ' . "\n\n" . 'After choose your items in our store, You can use it to enter ' . TEXT_GV_REDEEM . ' thing at the time of checkout.');
+define('EMAIL_GV_LINK', 'You can now redeem the links below.' . "\n");
 // GV link will automatically be included before this line
 
-define('EMAIL_GV_LINK_OTHER','お客様ご自身のアカウントに' . TEXT_GV_NAME . 'を追加しておけば、ご自分で' . TEXT_GV_NAME . 'をお使いいただけます。またお知り合いの方にプレゼントすることもできます。' . "\n\n");
+define('EMAIL_GV_LINK_OTHER','If ' . TEXT_GV_NAME . ' is added to the customer own account, ' . TEXT_GV_NAME . ' can be used for myself. You can also gift your acquaintance.' . "\n\n");
 
 define('EMAIL_TEXT',
- '当ショップにご登録いただいたアカウントで、お客様はこれから以下の<strong>便利な' . "\n" .
- 'サービス</strong>をご利用いただけます。' . "\n" .
+ 'Following convenient services can be used for the customer by the account registered in this shop.' . "\n" .
  "\n" .
- '・<strong>ショッピングカート</strong>' . "\n" .
- 'ショッピングカートに入れた商品は、削除または精算するまで' . "\n" .
- '保持しておくことができます。' . "\n" .
+ '・<strong>Shopping Cart</strong>' . "\n" .
+ 'Products put in shopping cart can be hold  until delete or checkout.' . "\n" .
  "\n" .
- '・<strong>アドレス帳</strong>' . "\n" .
- ' 贈り物などの際に便利なように、ご自宅の他にもお届け先を5件まで' . "\n" .
- '登録することができます。' . "\n" .
+ '・<strong>Address Book</strong>' . "\n" .
+ 'As a convenience to the gifts, Addressees can be registered up to five besides home.' . "\n" .
  "\n" .
- '・<strong>注文履歴</strong>' . "\n" .
- 'マイページから、当店でご注文いただいた商品の一覧' . "\n" .
- 'を確認することができます。' . "\n" .
+ '・<strong>Order History</strong>' . "\n" .
+ 'The list of the commodity ordered in our shop can be confirmed on a My Account.' . "\n" .
  "\n" .
- '・<strong>商品のレビュー</strong>' . "\n" .
- '当ショップの商品についてのレビュー(感想)を書き込んでいただくことができます。' . "\n" .
- '是非他のお客様に感想をお伝えください。' . "\n\n");
-define('EMAIL_CONTACT', '当ショップのオンラインサービスで何かご不明な点がございましたら、Eメールにてお気軽にお問い合わせ下さい: <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">'. STORE_OWNER_EMAIL_ADDRESS ." </a>\n\n");
-define('EMAIL_GV_CLOSURE','謹白' . "\n\n店長 " . STORE_OWNER . "\n\n". '<a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">'.HTTP_SERVER . DIR_WS_CATALOG ."</a>\n\n");
+ '・<strong>Product Reviews</strong>' . "\n" .
+ 'You can write a review (impression) for our store\'s products.' . "\n" .
+ 'Please tell your thoughts to other customers.' . "\n\n");
+define('EMAIL_CONTACT', 'If you have any questions on our store\'s online service, Please feel free to inquire by Email.: <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">'. STORE_OWNER_EMAIL_ADDRESS ." </a>\n\n");
+define('EMAIL_GV_CLOSURE','Yours truly,;' . "\n\nStore Owner " . STORE_OWNER . "\n\n". '<a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">'.HTTP_SERVER . DIR_WS_CATALOG ."</a>\n\n");
 
 // email disclaimer - this disclaimer is separate from all other email disclaimers
-define('EMAIL_DISCLAIMER_NEW_CUSTOMER', 'このメールアドレスは、お客様ご自身によって当ショップに登録されました。もしアカウント登録をされた覚えがない場合には、お手数ですが %s までご連絡ください。');
+define('EMAIL_DISCLAIMER_NEW_CUSTOMER', 'This mail address was registered in our store by the customer oneself. If you do not register the account, please contact %s.');
 
 //moved definitions to english.php
 //define('TABLE_HEADING_PRIVACY_CONDITIONS', 'Privacy Statement');
